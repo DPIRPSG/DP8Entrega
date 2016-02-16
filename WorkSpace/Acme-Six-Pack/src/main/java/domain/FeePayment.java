0cf1be2f7a2204e8d7af2872c326a0c4db88de2f
migrodcab <miguel.rc95@gmail.com>
2016-02-16 12:08:51 +0100

    Añadido amount a Uml/Domain/Populate

--- WorkSpace/Acme-Six-Pack/src/main/java/domain/FeePayment.java
+++ WorkSpace/Acme-Six-Pack/src/main/java/domain/FeePayment.java
@@ -9,13 +9,15 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
-public class FeePayment extends DomainEntity{
+public class FeePayment extends DomainEntity {
 
 	// Constructors -----------------------------------------------------------
 
@@ -24,68 +26,86 @@ public class FeePayment extends DomainEntity{
 	private CreditCard creditCard;
 	private Date activeMoment;
 	private Date inactiveMoment;
-	
+	private double amount;
+
 	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
 	@DateTimeFormat(pattern = "yyyy/dd/MM HH:mm")
 	public Date getPaymentMoment() {
 		return paymentMoment;
 	}
+
 	public void setPaymentMoment(Date paymentMoment) {
 		this.paymentMoment = paymentMoment;
 	}
-	
+
 	@NotNull
 	@Valid
 	public CreditCard getCreditCard() {
 		return creditCard;
 	}
+
 	public void setCreditCard(CreditCard creditCard) {
 		this.creditCard = creditCard;
 	}
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	@DateTimeFormat(pattern = "yyyy/dd/MM HH:mm")
 	public Date getActiveMoment() {
 		return activeMoment;
 	}
+
 	public void setActiveMoment(Date activeMoment) {
 		this.activeMoment = activeMoment;
 	}
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	@DateTimeFormat(pattern = "yyyy/dd/MM HH:mm")
 	public Date getInactiveMoment() {
 		return inactiveMoment;
 	}
+
 	public void setInactiveMoment(Date inactiveMoment) {
 		this.inactiveMoment = inactiveMoment;
 	}
-	
+
+	@NotNull
+	@Min(0)
+	@Digits(integer = 9, fraction = 2)
+	@Valid
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
 	// Relationships ----------------------------------------------------------
 
 	private Gym gym;
 	private Customer customer;
-	
+
 	@Valid
 	@NotNull
-	@ManyToOne(optional=false)
+	@ManyToOne(optional = false)
 	public Gym getGym() {
 		return gym;
 	}
+
 	public void setGym(Gym gym) {
 		this.gym = gym;
 	}
-	
+
 	@Valid
 	@NotNull
-	@ManyToOne(optional=false)
+	@ManyToOne(optional = false)
 	public Customer getCustomer() {
 		return customer;
 	}
+
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
-	
 
 }
