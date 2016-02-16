e440ada2b311fb39b19d0af795b2f1aeeb704441
migrodcab <miguel.rc95@gmail.com>
2016-02-16 18:00:59 +0100

    Añadida query

--- WorkSpace/Acme-Six-Pack/src/main/java/repositories/CustomerRepository.java
+++ WorkSpace/Acme-Six-Pack/src/main/java/repositories/CustomerRepository.java
@@ -1,6 +1,8 @@
 package repositories;
 
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,4 +15,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 	
 	@Query("select c from Customer c where c.userAccount.id = ?1")
 	Customer findByUserAccountId(int userAccountId);
-	}
+	
+	@Query("select distinct b.customer from Booking b join b.service s where s.id = ?1")
+	Collection<Customer> findByServiceBooked(int serviceId);
+}
