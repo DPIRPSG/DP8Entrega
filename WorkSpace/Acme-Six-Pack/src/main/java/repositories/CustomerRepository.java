10ea6170578c3615048d73042e5105a664e634de
migrodcab <miguel.rc95@gmail.com>
2016-02-16 18:22:39 +0100

    Corregido Fallo Gym-Booking

--- WorkSpace/Acme-Six-Pack/src/main/java/repositories/CustomerRepository.java
+++ WorkSpace/Acme-Six-Pack/src/main/java/repositories/CustomerRepository.java
@@ -16,6 +16,9 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 	@Query("select c from Customer c where c.userAccount.id = ?1")
 	Customer findByUserAccountId(int userAccountId);
 	
-	@Query("select distinct b.customer from Booking b join b.service s where s.id = ?1")
+	@Query("select distinct b.customer from Booking b where b.service.id = ?1")
 	Collection<Customer> findByServiceBooked(int serviceId);
+	
+	@Query("select distinct b.customer from Booking b where b.gym.id = ?1")
+	Collection<Customer> findByGymBooked(int gymId);
 }
