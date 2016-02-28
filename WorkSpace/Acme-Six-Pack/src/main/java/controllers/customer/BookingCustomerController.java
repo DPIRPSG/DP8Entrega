81deae88c601867621d31826f12560855d75cc89
guillermo-ag-95 <guillermo.ag.95@gmail.com>
2016-02-28 12:34:15 +0100

    Cambios menores
    - Cambio de ubicación del ActorForm con sus correspondientes import
    cambiados.
    - Cambio menor en populate.
    - Quitando cosas que no se usaban en el domain de Booking.
    - Controlando que booking no es null en el controller

--- WorkSpace/Acme-Six-Pack/src/main/java/controllers/customer/BookingCustomerController.java
+++ WorkSpace/Acme-Six-Pack/src/main/java/controllers/customer/BookingCustomerController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -99,6 +100,7 @@ public class BookingCustomerController extends AbstractController {
 		Booking booking;
 		
 		booking = bookingService.findOne(bookingId);
+		Assert.notNull(booking);		
 
 		try {
 			bookingService.cancel(booking);
